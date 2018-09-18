1bf4cc6f0929a13eed067b2ea9c3d8e201817cfa
Maria Ómarsdottir <mariaomarsdottir@gestur-043.ru.is>
2018-09-18 10:18:59 +0000

    Fyrstu skil

--- TileTraveller.py
+++ TileTraveller.py
@@ -2,17 +2,19 @@ x = 1
 y = 1
 
 while 1 <= x <= 3 and 1 <= y <=3:
-    direction = str(input(("Direction:")))
-    direction.lower()
 
-    if 1 <= x <= 3 and y == 1:
+    if 1 <= x <= 2 and y == 1:
         print("You can travel: (N)orth")
+        direction = str(input(("Direction:")))
+        direction.lower()
         if direction == 'n':
             y+=1
         else:
             print("Not a valid direction!")
     elif x == 1 and y == 2:
         print("You can travel: (N)orth or (E)ast or (S)outh.")
+        direction = str(input(("Direction:")))
+        direction.lower()
         if direction == 'n':
             y+=1
         elif direction == 's':
@@ -23,14 +25,18 @@ while 1 <= x <= 3 and 1 <= y <=3:
             print("Not a valid direction")
     elif x == 1 and y == 3:
         print("You can travel: (E)ast or (S)outh.")
+        direction = str(input(("Direction:")))
+        direction.lower()
         if direction == 's':
             y-=1
         elif direction == 'e':
             x+=1
         else:
             print("Not a valid direction!")
-    elif 2 <= x <= 3 and 2 <= y <= 3:
+    elif x == y and x != 1:
         print("You can travel: (S)outh or (W)est.")
+        direction = str(input(("Direction:")))
+        direction.lower()
         if direction == 's':
             y-=1
         elif direction == 'w':
@@ -39,6 +45,8 @@ while 1 <= x <= 3 and 1 <= y <=3:
             print("Not a valid direction!")
     elif x == 2 and y == 3:
         print("You can travel: (E)ast or (W)est.")
+        direction = str(input(("Direction:")))
+        direction.lower()
         if direction == 'e':
             x+=1
         elif direction == 'w':
@@ -47,14 +55,16 @@ while 1 <= x <= 3 and 1 <= y <=3:
             print("Not a valid direction!")
     elif x == 3 and y == 2:
         print("You can travel: (N)ourth or (S)outh.")
+        direction = str(input(("Direction:")))
+        direction.lower()
         if direction == 'n':
             y+=1
         elif direction == 's':
             y-=1
         else:
             print("Not a valid direction!")
-    else:
-        ("Not a valid direction!!!!")
+    elif x == 3 and y == 1:
+        break
 
         
 print("Victory!")
